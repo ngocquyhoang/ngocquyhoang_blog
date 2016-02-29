a41ad90a7131a9d48544205d5fd1fd2409116e09
ngocquyhoang <ngocquyhoang.93@gmail.com>
2016-02-29 11:15:10 +0700

    completed timeline (not reponsive)

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223091725) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "tasks", force: :cascade do |t|
-    t.string   "title"
-    t.text     "note"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
